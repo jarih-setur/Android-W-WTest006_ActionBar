a2f9b8634652de15ea1ccbf8f3236efcab4fd174
Jari í Hjøllum <jarih@setur.fo>
2020-03-03 15:32:53 +0000

    1st commit

---
+++ app/src/main/java/com/example/wtest006/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.wtest006;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        MenuItem item = (MenuItem) findViewById(R.id.check_for_updates);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+        case 0:  break;
+            default: System.out.println("Test is the default action");break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void action_settings_click() {
+
+    }
+
+
+}
