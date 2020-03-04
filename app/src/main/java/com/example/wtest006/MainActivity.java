732b1e5db360026d7f985201b48247a2a82dbc95
Jari í Hjøllum <jarih@setur.fo>
2020-03-04 15:46:21 +0000

    20200304 commit

--- app/src/main/java/com/example/wtest006/MainActivity.java
+++ app/src/main/java/com/example/wtest006/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.wtest006;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,12 +19,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
-
+        setSupportActionBar(toolbar); //SetActionBar to toolbar
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.miHelp:
+                Toast.makeText(getApplicationContext(),"Helping...", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.miSettings:
+                Toast.makeText(getApplicationContext(),"Settings", Toast.LENGTH_SHORT).show();
+                Intent settings = new Intent(this,SettingsActivity.class);
+                startActivity(settings);
+                break;
+            case R.id.miUpdates:
+                Toast.makeText(getApplicationContext(),"Latest version...", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                // Unknown option
+        }
+
         TextView textView = (TextView) findViewById(R.id.textView);
         String str = (String) item.getTitle();
         textView.setText("Last clicked item was: "+str.toString());
